fc0a18baaafa07ff25d516444b8bfffa5409bf3d
Camilo <juancmaringcu@gmail.com>
2024-04-18 20:18:23 -0500

    Se crea proyecto web y se define conexión con BD usando EntityFramework

---
+++ TiendaAlimentos.Adaptadores/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaAlimentos.Entidades;
+
+namespace TiendaAlimentos.Adaptadores;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    // Define otros DbSet para tus entidades aquí
+}
